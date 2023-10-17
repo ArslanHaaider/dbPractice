27f2046d26a3c9224f3f57a2d335eb80a7001f3a
Arslaan <aslanhaider707@gmail.com>
2023-10-17 16:55:22 +0500

    Add CRUD routes for courses and teacher

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controller/courses/courses');
+router.get('/',courseController.getAllCourse)
 router.post('/createCourse',courseController.createCourse);
+router.delete('/deleteCourse/:id', courseController.deleteCourse);
+router.put('/updateCourse/:id', courseController.updateCourse);
 module.exports = router;
--- routes/teachers.js
+++ routes/teachers.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const teacherController = require('../controller/teachers/teachers')
+router.get('/',teacherController.getAllTeacher);
 router.post('/addTeacher',teacherController.addTeacher);
+router.delete('/deleteTeacher/:id', teacherController.deleteTeacher);
+router.put('/updateTeacher/:id', teacherController.updateTeacher);
 module.exports = router;
