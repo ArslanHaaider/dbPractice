6b8da24f023e8dc0725c8ce87e256611bdb37443
Arslaan <aslanhaider707@gmail.com>
2023-10-17 16:59:54 +0500

    Add ,onUpdate:CASCADE for manyToMany relations

--- models/defination/index.js
+++ models/defination/index.js
@@ -6,10 +6,10 @@ let teacher = require('./teacher/teacher')
 
 user.hasOne(student,{onUpdate:"CASCADE",onDelete:"CASCADE",foreignKey:{name:'userID',allowNull:false,unique:true}})
 student.belongsTo(user,{onUpdate:"CASCADE",onDelete:"CASCADE",foreignKey:{name:'userID',allowNull:false,unique:true}})
-student.belongsToMany(courses,{through:'enrollStudent'});
-courses.belongsToMany(student,{through:'enrollStudent'});
-teacher.belongsToMany(courses,{through:'enrollTeacher'});
-courses.belongsToMany(teacher,{through:'enrollTeacher'});
+student.belongsToMany(courses,{through:'enrollStudent',onUpdate:"CASCADE"});
+courses.belongsToMany(student,{through:'enrollStudent',onUpdate:"CASCADE"});
+teacher.belongsToMany(courses,{through:'enrollTeacher',onUpdate:"CASCADE"});
+courses.belongsToMany(teacher,{through:'enrollTeacher',onUpdate:"CASCADE"});
 const models =sequelize.models;
 console.log(models);
 const db = {};
