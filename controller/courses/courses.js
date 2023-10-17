07e32eae1649051c966045399e095d45802d46a4
Arslaan <aslanhaider707@gmail.com>
2023-10-17 16:17:01 +0500

    Implement CURD for Courses

--- controller/courses/courses.js
+++ controller/courses/courses.js
@@ -5,5 +5,17 @@ module.exports = {
     createCourse: async (req,res) =>{
         const newCourse = await courseService.createCourse(req.body);
         res.send(newCourse);
-    }
+    },
+    updateCourse : async (req,res) =>{
+        const result = await courseService.updateCourse(req.params.id,req.body);
+        res.send(result);
+      },
+      getAllCourse: async(req,res) =>{
+        const courses = await courseService.getAllCourse();
+        res.send(courses);
+      },
+      deleteCourse: async (req,res)=>{
+        const deleted =  await courseService.deleteCourse(req.params.id);
+        res.send(deleted);
+      }
 }
--- services/courses.js
+++ services/courses.js
@@ -3,9 +3,37 @@ module.exports = {
     createCourse :async (data) =>{
       const result = await models.courses.create(data);
       return result
-    }
-  //   createCourseHelper: async (data) =>{
-  //     const newCourse = await models.courses.create(data);
-  //     return newCourse
-  // },
+    },
+    updateCourse:async (courseId ,data) =>{
+      try{
+          const teacher =await models.courses.findOne({where:{id:courseId }})
+          console.log(data);
+          if(teacher){
+              models.Student.update({experienceYears:data.experienceYears,Courses:data.Courses},{where:{id:courseId}})
+              return await models.teacher.findOne({where:{id:courseId }});
+          }
+      }catch(err){
+          console.log("no such Id exist for it to modiyg")
+          return "No such such student find that respective id"
+      }
+
+  },
+  getAllCourse:async() =>{
+      const Courses = await models.courses.findAll();
+      if (Courses){
+          return Courses
+      }else{
+          return "Can't find or asynchronus"
+      }
+  },
+  deleteCourse:async (courseId)=>{
+     const deleted = await models.courses.destroy({where:{id:courseId}})
+      if(deleted>0){
+          console.log("The number of deleted rows is " + deleted)
+          return "row deleted"
+      }
+      else{
+          return "couldn't find the specific student"
+      }
+  } 
 }
