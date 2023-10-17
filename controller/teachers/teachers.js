14100cb6f561bd6605faebf936e9f46f797cddf8
Arslaan <aslanhaider707@gmail.com>
2023-10-17 16:02:24 +0500

    Implement Student CURD functionality

--- controller/teachers/teachers.js
+++ controller/teachers/teachers.js
@@ -3,5 +3,17 @@ module.exports = {
     addTeacher :(req,res) => {
         const teacher = teacherService.createTeacher(req.body);
         res.send(teacher);
-    }
+    },
+    updateTeacher : async (req,res) =>{
+        const result = await teacherService.updateTeacher(req.params.id,req.body);
+        res.send(result);
+      },
+      getAllTeacher: async(req,res) =>{
+        const students = await teacherService.getAllTeacher();
+        res.send(students);
+      },
+      deleteTeacher: async (req,res)=>{
+        const deleted =  await teacherService.deleteTeacher(req.params.id);
+        res.send(deleted);
+      }
 }
--- services/teacher.js
+++ services/teacher.js
@@ -3,5 +3,37 @@ module.exports =  {
     createTeacher :(data) => {
         const teacher = models.teacher.create(data);
         return teacher;
-    }
+    },
+    updateTeacher:async (teacherId ,data) =>{
+        try{
+            const teacher =await models.teacher.findOne({where:{id:teacherId }})
+            console.log(data);
+            if(teacher){
+                models.Student.update({experienceYears:data.experienceYears,Courses:data.Courses},{where:{id:teacherId}})
+                return await models.teacher.findOne({where:{id:teacherId }});
+            }
+        }catch(err){
+            console.log("no such Id exist for it to modiyg")
+            return "No such such student find that respective id"
+        }
+
+    },
+    getAllTeacher:async() =>{
+        const Students = await models.Student.findAll();
+        if (Students){
+            return Students
+        }else{
+            return "Can't find or asynchronus"
+        }
+    },
+    deleteTeacher:async (teacherId)=>{
+       const deleted = await models.teacher.destroy({where:{id:teacherId}})
+        if(deleted>0){
+            console.log("The number of deleted rows is " + deleted)
+            return "row deleted"
+        }
+        else{
+            return "couldn't find the specific student"
+        }
+    } 
 }
